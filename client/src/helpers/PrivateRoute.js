fa00ae1eeac191519685a79275a499d882055fef
Einar Sigurðsson <einarsi@landspitali.is>
2021-08-23 12:29:41 +0000

    ADD: added check in PrivateRoute to check if token is expired

--- client/src/helpers/PrivateRoute.js
+++ client/src/helpers/PrivateRoute.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import NavBar from '../features/NavBar'
+import jwtDecode from 'jwt-decode'
+
+// Check if token and clear if it is expired
+const tokenExpired = () => {
+  const token = window.localStorage.getItem('token') || null
+
+  if (token) {
+    const decodedToken = jwtDecode(token)
+
+    if (decodedToken.exp * 1000 < new Date().getTime()) {
+      window.localStorage.removeItem('token')
+    }
+  }
+
+  return token
+}
+
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={(props) => localStorage.getItem('token')
+    render={(props) => tokenExpired()
       ? (
         <>
           <NavBar />
